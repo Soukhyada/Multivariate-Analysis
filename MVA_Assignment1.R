c6b26e3996e78de424232f0d2a8a8e99ebfcc91e
Soukhyada <47436417+Soukhyada@users.noreply.github.com>
2019-02-08 13:38:04 -0500

    Add files via upload

---
+++ MVA_Assignment1.R
@@ -0,0 +1,10 @@
+#Soukhyada Vaidya
+#Assignment 1: Facebook Ad Analysis
+#Loading the data
+Fb <- read.csv("C:/Users/Soukhyada/Desktop/Rutgers Fall'18/SEM 2/Multivariate/KAG_conversion_data.csv")
+#View the data
+View(Fb)
+#Displays the first few rows of the dataset
+head(Fb)
+#Display the structure of the attributes
+str(Fb)
